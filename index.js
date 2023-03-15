bb565f29feb1fd9cfb37fd67fa8021de33c6b34f
motaz <motaz.dn.ali@gmail.com>
2023-03-15 13:21:21 +0300

    PHASE-1-ARITHMETIC-LAB

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 10;
+const num4 = 6;
+
+const set = new Set([2, 3, 15, 17, 20, 7]);
+//const arr = [2, 3, 15, 17, 20, 7];
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const mod = num3 % num4;
+
+const max = Math.max(...set);
+
+console.log(max);
